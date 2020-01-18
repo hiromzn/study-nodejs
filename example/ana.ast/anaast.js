a661c4562fff232de1b1c0cf2d5e4c0546ca6b6b
Hiromichi Mizuno <hiromzn@gmail.com>
2020-01-18 11:10:25 +0900

    mod

--- example/ana.ast/anaast.js
+++ example/ana.ast/anaast.js
@@ -1,38 +1,86 @@
+"use strict";
+
+const debug = false;
+
 const fs = require('fs');
-let fname = "test.h.ast";
+const readline = require('readline');
+const glob = require('glob');
 
-// let regexp = /t(e)(st(\d?))/g;
-// let str = 'test1test2';
-// let array = [...str.matchAll(regexp)];
-// console.log(array);
-
-var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream(fname)
-});
-  
-lineReader.on('line', function (line) {
-	let regex = /\-/;
-	let level = (line.search(regex) + 1)/2;
-	let lastcp = level*2-2;
-	let islast = ( line.substring(lastcp,lastcp+1) == '`' );
-	let res1 = [...line.matchAll( /-RecordDecl\s+(\S+)\s+<.*>\s+\S+\s+(struct\s+\S+)\sdefinition/ )];
-	console.log(res1);
-	let res2 = [...line.matchAll( /-FieldDecl\s+(\S+)\s+<.*>\s+\S+\s+(\S+)\s+'(.+)'/ )];
-	console.log(res2);
-	let res3 = [...line.matchAll( /-TypedefDecl\s+(\S+)\s+<.*>\s+\S+\s+(\S+)\s+'(.+)':'(.+)'/ )];
-	console.log(res3);
-	// let type = res[1];
-	// let id = res[2];
-	// let slocd = res[1];
-    console.log('Line from file:', islast, level, line);
-});
+// main
+glob('data/*.ast', (error, files) => {
+	if (error) throw error;
+	console.log(`DONE : All files: ${JSON.stringify(files)}`);
+})
+		.on('match', file => {
+			console.log(`check file >>>${file}`);
+			parse_file( file );
+		});
 
 
-// |-RecordDecl 0x7fd34c030f48 <test.h:1:9, line:5:1> line:1:16 struct my_test definition
-// | |-FieldDecl 0x7fd34c087c00 <line:2:2, col:7> col:7 i 'int'
-// | |-FieldDecl 0x7fd34c087c60 <line:3:2, col:8> col:8 p 'char *'
-// | `-FieldDecl 0x7fd34c087cc0 <line:4:2, col:7> col:7 l 'long'
-// `-TypedefDecl 0x7fd34c087d68 <line:1:1, line:5:3> col:3 MY_TEST 'struct my_test':'struct my_test'
-//   `-ElaboratedType 0x7fd34c087d10 'struct my_test' sugar
-//     `-RecordType 0x7fd34c030fc0 'struct my_test'
-//       `-Record 0x7fd34c030f48 'my_test'
+const structs = [];
+const typedefs = [];
+let current_struct = null;
+
+function parse_file( file )
+{
+	const lineReader = readline.createInterface({input : fs.createReadStream( file )});
+	lineReader.on( 'line', function( line ) {
+		pattern( line );
+	});
+	lineReader.on( 'close', function( line ) {
+		console.log( "###### FILE :%s", file );
+		console.log( JSON.stringify(structs, null, '\t') );
+		console.log( JSON.stringify(typedefs, null, '\t') );
+	});
+}
+
+function pattern( line )
+{
+	const level_regex = /\-/;
+	const level = (line.search(level_regex) + 1)/2;
+
+	const lastcp = level*2-2;
+	const islast = ( line.substring(lastcp,lastcp+1) == '`' );
+
+    //console.log('Line info:', islast, level );
+
+	let res;
+	if ( res = line.match( /-RecordDecl\s+(\S+)\s+<.*>\s+.*(struct\s+\S+)\sdefinition/ ) ) {
+		current_struct = {
+			level : level,
+			astid : res[1],
+			name  : res[2],
+			member : []
+		};
+		structs.push( current_struct );
+		if (debug) console.log( "RecordDecl:%s", current_struct );
+	}
+
+	if ( (res = line.match( /-FieldDecl\s+(\S+)\s+<.*>\s+\S+\s+(\S+)\s+'(.+)'/ )) ||
+		 (res = line.match( /-FieldDecl\s+(\S+)\s+<.*>\s+.*(\S+)\s+'(.+)'/ )) ) {
+		let mem = {
+			level : level,
+			astid : res[1],
+			name  : res[2],
+			type  : res[3]
+		};
+		if (current_struct) {
+			current_struct.member.push( mem );
+			if (debug) console.log( "FieldDecl:%s", mem );
+			if (islast) current_struct = null;
+		}
+	}
+
+	if ( (res = line.match( /-TypedefDecl\s+(\S+)\s+<.*>\s+\S+\s+(\S+)\s+'(.+)':'(.+)'/ )) ||
+		 (res = line.match( /-TypedefDecl\s+(\S+)\s+<.*>\s+.*(\S+)\s+'(.+)':'(.+)'/ )) ) {
+		let tdef = {
+			level : level,
+			astid : res[1],
+			name  : res[2],
+			org   : res[3],
+			org2  : res[4]
+		};
+		typedefs.push( tdef );
+		if (debug) console.log( "TypedefDecl:%s", tdef );
+	}
+}
